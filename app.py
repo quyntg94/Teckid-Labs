2b5d4772bc450c2278388db2424badf6aacfe2bd
quynth94 <giaquy.ptit@gmail.com>
2016-10-19 20:49:58 +0700

    push

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
+post1 = {
+    "title" : "Good day",
+    "content" : "Today, i met a girl. She has black eyes and loves ice-cream"
+}
+
+post2 = {
+    "title" : "Bad day",
+    "content" : "Today, i met a boy. He has black eyes and loves ice-cream"
+}
+
+print(post1("title"))
+print(post1("content"))
+
+print(post2("title"))
+print(post2("content"))
+
 @app.route('/')
 def hello_world():
-    print('Hello world')
-    return 'Hello world'
+    return json.dumps(post1)
 
 if __name__=='__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=1607)
