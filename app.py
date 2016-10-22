9da69b48db5a2fc326ed239ed83671dd1d0fdde3
quynth94 <giaquy.ptit@gmail.com>
2016-10-22 20:22:54 +0700

    pussh

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-import json
-from flask import Flask
+from flask import *
+from mlab import *
+from mongoengine import *
 
 app = Flask(__name__)
+mlab_connect()
 
-post1 = {
-    "title" : "Good day",
-    "content" : "Today, i met a girl. She has black eyes and loves ice-cream"
-}
-
-post2 = {
-    "title" : "Bad day",
-    "content" : "Today, i met a boy. He has black eyes and loves ice-cream"
-}
-
-print(post1["title"])
-print(post1["content"])
+class Post(Document):
+    title = StringField()
+    content = StringField()
+    def to_json(self):
+        return {"title": self.title, "content": self.content}
 
 @app.route('/')
 def hello_world():
-    return json.dumps(post1)
+    return jsonify([post.to_json() for post in Post.objects])
 
-if __name__=='__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=3000)
+if __name__ == '__main__':
+    app.run()
